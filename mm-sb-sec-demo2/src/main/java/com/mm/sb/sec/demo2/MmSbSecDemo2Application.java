7ab3a8e1dd4eebfae9130b1e748476016c0cd9a7
Sekhar Babu Patnam <babuthetrainer@gmail.com>
2021-03-27 21:06:58 +0530

    Spring boot security demo2 application

---
+++ mm-sb-sec-demo2/src/main/java/com/mm/sb/sec/demo2/MmSbSecDemo2Application.java
@@ -0,0 +1,13 @@
+package com.mm.sb.sec.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmSbSecDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmSbSecDemo2Application.class, args);
+	}
+
+}
---
+++ mm-sb-sec-demo2/src/test/java/com/mm/sb/sec/demo2/MmSbSecDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.sb.sec.demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmSbSecDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
