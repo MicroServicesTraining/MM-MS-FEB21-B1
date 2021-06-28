d25e42a771755f19226d4f61a46dbd3eee5260e7
Sekhar Babu Patnam <35616006+sekharbabu4patnam@users.noreply.github.com>
2021-06-28 06:58:55 +0530

    Update MmEcomDemoApplication.java

--- mm-ecom-demo/src/main/java/com/mm/ecom/MmEcomDemoApplication.java
+++ mm-ecom-demo/src/main/java/com/mm/ecom/MmEcomDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MmEcomDemoApplication {
-
+	//soem comment
 	public static void main(String[] args) {
 		SpringApplication.run(MmEcomDemoApplication.class, args);
 	}
